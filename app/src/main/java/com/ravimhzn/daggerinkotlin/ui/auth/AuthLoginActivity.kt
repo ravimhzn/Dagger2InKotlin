a312e15b223fb73429d010ffcab57464b255e221
Ravi Maharjan <ravi.maharjan2018@gmail.com>
2020-02-09 19:08:13 +1100

    LiveData Reactive Streams (MediatorLive Data with and Without)

--- app/src/main/java/com/ravimhzn/daggerinkotlin/ui/auth/AuthLoginActivity.kt
+++ app/src/main/java/com/ravimhzn/daggerinkotlin/ui/auth/AuthLoginActivity.kt
@@ -2,22 +2,29 @@ package com.ravimhzn.daggerinkotlin.ui.auth
 
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.RequestManager
+import com.google.android.material.textfield.TextInputEditText
 import com.ravimhzn.daggerinkotlin.R
+import com.ravimhzn.daggerinkotlin.models.User
 import dagger.android.AndroidInjection
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
-class AuthLoginActivity : DaggerAppCompatActivity() {
+class AuthLoginActivity : DaggerAppCompatActivity(), View.OnClickListener {
 
     private val TAG = "AuthLoginActivity::"
 
     @Inject
     lateinit var providerFactory: ViewModelProvider.Factory
 
-    lateinit var authLoginActivityViewModel: AuthLoginViewModel
+    lateinit var viewModel: AuthLoginViewModel
 
     @Inject
     lateinit var requestManager: RequestManager
@@ -27,22 +34,66 @@ class AuthLoginActivity : DaggerAppCompatActivity() {
 
     private lateinit var imgLogo: ImageView
 
+    private lateinit var etUserId: TextInputEditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        authLoginActivityViewModel =
-            ViewModelProvider(this, providerFactory).get(AuthLoginViewModel::class.java)
-
-
         initiateViewModel()
+        initViews()
+        subscribeToObservers()
+    }
+
+    private fun initViews() {
+        etUserId = findViewById(R.id.user_id_input)
         imgLogo = findViewById<ImageView>(R.id.imgLoginLogo)
         requestManager.load(dLogo).into(imgLogo)
+        findViewById<Button>(R.id.btn_login).setOnClickListener(this)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.observeUser().observe(this, object : Observer<User> {
+            override fun onChanged(t: User?) {
+                t?.let {
+                    Log.d(TAG, "EMAIL:::: ${t.email}")
+                }
+            }
+        })
     }
 
     private fun initiateViewModel() {
+        viewModel =
+            ViewModelProvider(this, providerFactory).get(AuthLoginViewModel::class.java)
+    }
 
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.btn_login -> {
+                attemptLogin()
+            }
+        }
+    }
+
+    private fun attemptLogin() {
+        if (TextUtils.isEmpty(etUserId.text.toString())) {
+            etUserId.error = "User Id cannot be empty"
+            return
+        }
+
+        viewModel.authenticateWithMediatorLiveDataId(Integer.parseInt(etUserId.text.toString()))
+
+        //withoutUsingMediatorLiveData()
+    }
+
+    private fun withoutUsingMediatorLiveData() {
+        viewModel.authenticateWithId(Integer.parseInt(etUserId.text.toString()))
+            .observe(this, object : Observer<User> {
+                override fun onChanged(t: User?) {
+                    Log.d(TAG, "EMAIL::: (WITHOUT MLV) ${t?.email}")
+                }
+
+            })
     }
 }
--- app/src/main/java/com/ravimhzn/daggerinkotlin/ui/auth/AuthLoginViewModel.kt
+++ app/src/main/java/com/ravimhzn/daggerinkotlin/ui/auth/AuthLoginViewModel.kt
@@ -1,47 +1,80 @@
 package com.ravimhzn.daggerinkotlin.ui.auth
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.ViewModel
 import com.ravimhzn.daggerinkotlin.models.User
 import com.ravimhzn.daggerinkotlin.network.AuthLoginAPI
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class AuthLoginViewModel @Inject constructor(authLoginAPI: AuthLoginAPI) : ViewModel() {
 
-    private val TAG : String = "AuthLoginViewModel::"
+    private val TAG: String = "AuthLoginViewModel::"
+    private lateinit var authLoginAPI: AuthLoginAPI
+
+    private var authUser = MediatorLiveData<User>()
+
     init {
-        authLoginAPI.getUser(5)
-            .toObservable()
-            .subscribeOn(Schedulers.io())
-            .subscribe(object : Observer<User> {
-                override fun onComplete() {
+        this.authLoginAPI = authLoginAPI
+    }
 
-                }
+    /**
+     * Get User Information from Server using RxJava and
+     * Use ViewModel - LiveData to communicate with UI
+     */
+    fun authenticateWithMediatorLiveDataId(userId: Int) {
+        var source = LiveDataReactiveStreams.fromPublisher(
+            authLoginAPI.getUser(userId)
+                .subscribeOn(Schedulers.io())
+        )
 
-                override fun onSubscribe(d: Disposable) {
+        //Converting the source to MediatorLiveData
+        authUser.addSource(source) {
+            authUser.value = it
+            authUser.removeSource(source)
+        }
+    }
 
-                }
+    fun observeUser(): LiveData<User> {
+        return authUser
+    }
 
-                override fun onNext(t: User) {
-                    Log.d(TAG, "EMAIL :: ${t.email}")
-                }
 
-                override fun onError(e: Throwable) {
-                    Log.d(TAG, "ERROR OCCURRED :: $e")
-                }
-            })
+//    fun testHttpConnectionWithRetrofit() {
+//        authLoginAPI.getUser(5)
+//            .toObservable()
+//            .subscribeOn(Schedulers.io())
+//            .subscribe(object : Observer<User> {
+//                override fun onComplete() {
+//
+//                }
+//
+//                override fun onSubscribe(d: Disposable) {
+//
+//                }
+//
+//                override fun onNext(t: User) {
+//                    Log.d(TAG, "EMAIL :: ${t.email}")
+//                }
+//
+//                override fun onError(e: Throwable) {
+//                    Log.d(TAG, "ERROR OCCURRED :: $e")
+//                }
+//            })
+//    }
 
-//        authLoginAPI.getUser(1)
-//            .toObservable() // extension function for Iterables
-//            .subscribeBy(  // named arguments for lambda Subscribers
-//                onNext = { println(it) },
-//                onError =  { it.printStackTrace() },
-//                onComplete = { println("Done!") }
-//            )
 
+    /**
+     * Workaround without the use of MediatorLiveData
+     */
+    fun authenticateWithId(userId: Int): LiveData<User> {
+        return LiveDataReactiveStreams.fromPublisher(
+            authLoginAPI.getUser(userId).subscribeOn(
+                Schedulers.io()
+            )
+        )
     }
 }
 
