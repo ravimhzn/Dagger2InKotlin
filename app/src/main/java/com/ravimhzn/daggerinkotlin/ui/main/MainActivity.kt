2447c6408986f0c0b6188b6f118ba823acd5d0d0
Ravi Maharjan <ravi.maharjan2018@gmail.com>
2020-02-17 16:04:03 +1100

    Android Navigation via Jetpack configured and added

--- app/src/main/java/com/ravimhzn/daggerinkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/ravimhzn/daggerinkotlin/ui/main/MainActivity.kt
@@ -4,29 +4,48 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.navigation.NavigationView
 import com.ravimhzn.daggerinkotlin.BaseActivity
 import com.ravimhzn.daggerinkotlin.R
-import com.ravimhzn.daggerinkotlin.ui.main.post.PostFragment
 
-class MainActivity : BaseActivity() {
 
-    val TAG = MainActivity::class.java.name
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val TAG = MainActivity::class.java.name
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        testFragment()
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+        navigationSetUp()
     }
 
-    private fun testFragment() {
-        supportFragmentManager.beginTransaction()
-            .replace(
-                R.id.main_container,
-                PostFragment()
-            )
-            .commit()
+    private fun navigationSetUp() {
+        val navController =
+            Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navigationView, navController)
+        navigationView.setNavigationItemSelectedListener(this)
     }
+
+//    private fun testFragment() {
+//        supportFragmentManager.beginTransaction()
+//            .replace(
+//                R.id.main_container,
+//                PostFragment()
+//            )
+//            .commit()
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
@@ -41,9 +60,71 @@ class MainActivity : BaseActivity() {
                 sessionManager.logout()
                 true
             }
+
+            android.R.id.home -> {
+                //Opens and Closes Hamburger Icon
+                return if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                    true
+                } else {
+                    false
+                }
+            }
+
             else -> {
                 super.onOptionsItemSelected(item)
             }
+        }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_profile -> {
+
+                // nav options to clear backstack
+                val navOptions = NavOptions.Builder()
+                    .setPopUpTo(R.id.main, true)
+                    .build()
+
+                Navigation.findNavController(this, R.id.nav_host_fragment)
+                    .navigate(R.id.profileScreen, null, navOptions)
+            }
+            R.id.nav_posts -> {
+                if (isValidDestination(R.id.postsScreen)) {
+                    Navigation.findNavController(this, R.id.nav_host_fragment)
+                        .navigate(R.id.postsScreen)
+                }
+            }
+        }
+        menuItem.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    /**
+     * This will check if a user is clicking on same menu multiple times
+     * in order to prevent stacking up fragment
+     */
+    private fun isValidDestination(destination: Int): Boolean {
+        return destination != Navigation.findNavController(
+            this,
+            R.id.nav_host_fragment
+        ).currentDestination!!.id
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_fragment),
+            drawerLayout
+        )
+    }
+
+    override fun onBackPressed() {
+        //On Back Pressed...Opens and Closes Hamburger Icon
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 }
